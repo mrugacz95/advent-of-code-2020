243b30f903572e01151846177698c6dcaaba7264
Marcin Mrugas <marcin.mrugas@allegro.pl>
2020-12-01 08:43:05 +0100

    Day 1 solution 🎅

---
+++ src/main/kotlin/pl/mrugacz95/aoc/day1.kt
@@ -0,0 +1,27 @@
+package pl.mrugacz95.aoc
+
+fun solution(numbers: List<Int>) {
+    for (i in numbers.indices) {
+        val a = numbers[i]
+        for (j in i until numbers.size) {
+            val b = numbers[j]
+            if (a + b == 2020) {
+                println("Part 1 answer: $a*$b=${a * b}")
+            }
+            for (k in j until numbers.size) {
+                val c = numbers[k]
+                if (a + b + c == 2020) {
+                    println("Part 2 answer: $a*$b*$c=${a * b * c}")
+                }
+            }
+        }
+    }
+}
+
+fun main() {
+    val numbers = {}::class.java.getResource("/day1.in")
+        .readText()
+        .split("\n")
+        .map { x -> x.toInt() }
+    solution(numbers)
+}
