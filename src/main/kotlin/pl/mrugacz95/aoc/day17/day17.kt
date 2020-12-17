883372de22d2295cd98b3249fd866e46a3ec3b6a
Marcin Mrugas <marcin.mrugas@allegro.pl>
2020-12-17 23:04:19 +0100

    Solution for day 17 part 2 🍪

--- src/main/kotlin/pl/mrugacz95/aoc/day17/day17.kt
+++ src/main/kotlin/pl/mrugacz95/aoc/day17/day17.kt
@@ -1,123 +1,137 @@
 package pl.mrugacz95.aoc.day17
 
-fun <T, U> Iterable<T>.cartesianProduct(other: Iterable<U>): Iterable<Pair<T, U>> {
-    return this.flatMap { lhsElem -> other.map { rhsElem -> lhsElem to rhsElem } }
+fun <T> Iterable<Iterable<T>>.cartesianProduct(other: Iterable<Iterable<T>>): List<List<T>> {
+    return this.flatMap { lhsElem -> other.map { rhsElem -> lhsElem + rhsElem } }
 }
 
-typealias Cube = Triple<Int, Int, Int>
+fun mixRanges(vararg ranges: IntRange): List<List<Int>> {
+    return ranges.map { range -> range.map { listOf(it) } }
+        .reduce { acc, i -> acc.cartesianProduct(i) }
+}
 
-val Cube.x: Int
-    get() = first
+typealias Cube = List<Int>
 
-val Cube.y: Int
-    get() = second
-
-val Cube.z: Int
-    get() = third
-
-val Cube.w: Int
-    get() = third
-
-
-const val ACTIVE = '#'
-const val INACTIVE = '.'
-var state = {}::class.java.getResource("/day17.in")
-    .readText()
-    .split("\n")
-    .withIndex()
-    .map { row ->
-        row.value.withIndex()
-            .filter { field -> field.value == ACTIVE }
-            .map { Triple(it.index, row.index, 0) }
-    }
-    .flatten()
-    .toSet()
-
-var xRange = 0..(state.maxByOrNull { it.first }?.first ?: throw RuntimeException("Wrong input"))
-var yRange = 0..(state.maxByOrNull { it.second }?.second ?: throw RuntimeException("Wrong input"))
-var zRange = 0..0
-var neighbourhood = (-1..1)
-    .cartesianProduct(-1..1)
-    .cartesianProduct(-1..1)
-    .filterNot { it.first.first == 0 && it.second == 0 && it.first.second == 0 }
-    .map { Triple(it.first.first, it.first.second, it.second) }
+fun Cube(vararg values: Int): Cube {
+    return values.toList()
+}
 
 operator fun Cube.plus(other: Cube): Cube {
-    return Cube(first + other.first, second + other.second, third + other.third)
+    return zip(other).map { it.first + it.second }
 }
 
-fun countNeighbours(cube: Cube, state: Set<Cube>): Int {
-    var count = 0
-    for (neighbour in neighbourhood) {
-        if (cube + neighbour in state) {
-            count += 1
+class GameOfLife(private val dimensions: Int, input: List<List<Boolean>>) {
+
+    companion object {
+        const val ACTIVE = '#'
+        const val INACTIVE = '.'
+    }
+
+    private var state = input
+        .withIndex()
+        .map { row ->
+            row.value.withIndex()
+                .filter { field -> field.value }
+                .map { Cube(it.index, row.index, *IntArray(dimensions - 2) { 0 }) }
+        }
+        .flatten()
+        .toSet()
+
+    private var ranges = List(dimensions) { dim ->
+        when (dim) {
+            0    -> 0..(state.maxByOrNull { it[dim] }?.get(dim) ?: throw RuntimeException("Wrong input"))
+            1    -> 0..(state.maxByOrNull { it[dim] }?.get(dim) ?: throw RuntimeException("Wrong input"))
+            else -> 0..0
         }
     }
-    return count
-}
 
-fun IntRange.update(value: Int): IntRange {
-    return minOf(first, value)..maxOf(last, value)
-}
+    var neighbourhood = mixRanges(*Array(dimensions) { (-1..1) })
+        .filterNot { it.all { v -> v == 0 } }
 
-fun IntRange.increase(): IntRange {
-    return (first - 1)..(last + 1)
-}
-
-fun iterValues() = sequence {
-    for (z in zRange.increase()) {
-        for (y in yRange.increase()) {
-            for (x in xRange.increase()) {
-                yield(Cube(x, y, z))
+    private fun countNeighbours(cube: Cube, state: Set<Cube>): Int {
+        var count = 0
+        for (neighbour in neighbourhood) {
+            if (cube + neighbour in state) {
+                count += 1
             }
         }
+        return count
     }
-}
 
-fun step(state: Set<Cube>): HashSet<Cube> {
-    val nextCubes = HashSet<Cube>()
-    var nextXRange = xRange
-    var nextYRange = yRange
-    var nextZRange = zRange
-    for (cube in iterValues()) {
-        val active = when (countNeighbours(cube, state)) {
-            2    -> cube in pl.mrugacz95.aoc.day17.state
-            3    -> true
-            else -> false
-        }
-        if (active) {
-            nextCubes.add(cube)
-            nextXRange = nextXRange.update(cube.x)
-            nextYRange = nextYRange.update(cube.y)
-            nextZRange = nextZRange.update(cube.z)
+    private fun IntRange.update(value: Int): IntRange {
+        return minOf(first, value)..maxOf(last, value)
+    }
+
+    private fun IntRange.increase(): IntRange {
+        return (first - 1)..(last + 1)
+    }
+
+    private fun iterValues() = sequence {
+        for (cube in mixRanges(*ranges.map { it.increase() }.toTypedArray())) {
+            yield(cube)
         }
     }
-    xRange = nextXRange
-    yRange = nextYRange
-    zRange = nextZRange
-    return nextCubes
-}
 
-fun printState() {
-    for (z in zRange) {
-        println("z=$z")
-        for (y in yRange) {
-            for (x in xRange) {
-                print(if (Triple(x, y, z) in state) ACTIVE else INACTIVE)
+    fun step() {
+        val nextCubes = HashSet<Cube>()
+        var nextRanges = ranges
+        for (cube in iterValues()) {
+            val active = when (countNeighbours(cube, state)) {
+                2    -> cube in state
+                3    -> true
+                else -> false
             }
-            println()
+            if (active) {
+                nextCubes.add(cube)
+                nextRanges = nextRanges.zip(cube).map {
+                    it.first.update(it.second)
+                }
+            }
         }
+        ranges = nextRanges
+        state = nextCubes
     }
-    println()
+
+    fun printState() {
+        if (dimensions > 4) return // skip
+        val outerRange = ranges.getOrElse(3) { 0..0 }
+        for (w in outerRange) {
+            for (z in ranges[2]) {
+                println("z=$z, w=$w")
+                for (y in ranges[1]) {
+                    for (x in ranges[0]) {
+                        if (dimensions > 3)
+                            print(if (listOf(x, y, z, w) in state) ACTIVE else INACTIVE)
+                        else
+                            print(if (listOf(x, y, z) in state) ACTIVE else INACTIVE)
+                    }
+                    println()
+                }
+            }
+        }
+        println()
+    }
+
+    fun activeCells(): Int {
+        return state.count()
+    }
 }
 
-fun part1(): Int {
+fun solve(dimensions: Int, input: List<List<Boolean>>): Int {
+    val simulation = GameOfLife(dimensions, input)
     for (i in 1..6) {
-        state = step(state)
+//        simulation.printState()
+        simulation.step()
     }
-    return state.count()
+    return simulation.activeCells()
 }
 
 fun main() {
-    println("Answer part 1: ${part1()}")
+    val input = {}::class.java.getResource("/day17.in")
+        .readText()
+        .split("\n")
+        .map { row ->
+            row.map { it == GameOfLife.ACTIVE }
+        }
+    println("Answer part 1: ${solve(3, input)}")
+    println("Answer part 2: ${solve(4, input)}")
 }
